2ae6c404191f750d5a887b50befa4c3a1d12f073
Roma Levsha <rm.levsha@gmail.com>
2021-02-09 10:02:27 +0300

    fix: correct function toggleLanguage

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function toggleLanguage() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById('myDropdown').classList.toggle('show');
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
+    const dropdowns = document.getElementsByClassName('dropdown-content');
 
-    const dropdowns = document.getElementsByClassName("dropdown-content");
-    
     for (let i = 0; i < dropdowns.length; i++) {
       let openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
