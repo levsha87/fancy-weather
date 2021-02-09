cfed9341dd30c306bf26e4b5a070100c60809422
Roma Levsha <rm.levsha@gmail.com>
2021-02-09 10:00:57 +0300

    feat: add function getCurrentFullTime

--- js/main.js
+++ js/main.js
@@ -19,18 +19,46 @@ window.onclick = function(event) {
   }
 };
 
-  // Initialize and add the map
- function initMap() {
+// Initialize and add the map
+function initMap() {
   // The location of Minsk
   const minsk = { lat: 53.8962037, lng: 27.5504956 };
   // The map, centered at minsk
-  const map = new google.maps.Map(document.querySelector(".user-location__geolocation"), {
-    zoom: 7,
-    center: minsk,
-  });
+  const map = new google.maps.Map(
+    document.querySelector('.user-location__geolocation'),
+    {
+      zoom: 7,
+      center: minsk,
+    }
+  );
   // The marker, positioned at minsk
   const marker = new google.maps.Marker({
     position: minsk,
     map: map,
   });
-}  
+}
+/*--------------------get current month, day, time---------------*/
+function getCurrentFullTime() {
+  const monthEl = document.querySelector('.month');
+  const dateTodayEl = document.querySelector('.date-today');
+  const dayTodayEl = document.querySelector('.day-today');
+  const timeEl = document.querySelector('.time');
+
+  setInterval(() => {
+    let today = new Date();
+    const arr = today.toString().split(' ');
+    arr.length = 5;
+
+    let month = arr[1];
+    let dateToday = arr[2];
+    let dayToday = arr[0];
+    let timeNow = arr[4];
+
+    monthEl.innerHTML = month;
+    dateTodayEl.innerHTML = dateToday;
+    dayTodayEl.innerHTML = dayToday;
+    timeEl.innerHTML = timeNow;
+  }, 1000);
+}
+
+getCurrentFullTime();
