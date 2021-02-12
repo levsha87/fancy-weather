9f27c0427bfb02d77d60cde69e7f65a13069f56c
Roma Levsha <rm.levsha@gmail.com>
2021-02-12 23:00:37 +0300

    fix: changed way choosing language

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function toggleLanguage() {
-  document.getElementById('myDropdown').classList.toggle('show');
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    const dropdowns = document.getElementsByClassName('dropdown-content');
-
-    for (let i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-};
-
 // Initialize and add the map
 function initMap(latitudeCurrentCity, longitudeCurrentCity) {
   console.log(latitudeCurrentCity, longitudeCurrentCity);
@@ -362,15 +342,33 @@ async function changeBackkgroundImage() {
 
 function changeBackgroundHandly() {
   let refreshButton = document.querySelector('.refresh-button');
-  refreshButton.addEventListener('click', changeBackkgroundImageRefreshButton);
-  refreshButton.addEventListener('animationend', changeBackkgroundImageRefreshButtonAnimationend);
+  refreshButton.addEventListener('click', changeBackkgroundImageClickRefreshButton);
+  refreshButton.addEventListener('animationend', changeBackkgroundImageRefreshButtonAnimationEnd);
 
-  function changeBackkgroundImageRefreshButton() {
+  function changeBackkgroundImageClickRefreshButton() {
     refreshButton.classList.add('active');
     changeBackkgroundImage();
   }
 
-  function changeBackkgroundImageRefreshButtonAnimationend() {
+  function changeBackkgroundImageRefreshButtonAnimationEnd() {
     refreshButton.classList.remove('active');
   }
 }
+
+function changeTemperatureUnit (temp, currentTemperature){
+  let buttons = document.querySelector('.temperature-buttons');
+  console.log(buttons);
+  
+  buttons.addEventListener("change", function(event) {
+    if(!event.target.checked && event.target.value ==='C'){
+      let item = event.target.checked;
+    console.log(item, event, event.isTrusted);
+    far = Math.trunc((temp * 9 / 5) + 32);
+    currentTemperature.innerHTML = far;
+    }
+    
+  });
+}
+
+  
+
