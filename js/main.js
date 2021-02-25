675cdb774e4f538d40d61cc468d2c01a3442c560
Roma Levsha <rm.levsha@gmail.com>
2021-02-25 12:58:03 +0300

    feat: add file with wind direction describe, correct function translateValueWindDirectionDegToCard

--- js/main.js
+++ js/main.js
@@ -194,7 +194,7 @@ function getCoordinateCurrentCityNavigator() {
 async function getCoordinateIpAdress () {
   const response = await fetch('https://ipinfo.io/json?token=543fd5d393e868');
     const place = await response.json();
-    
+    console.log(place);
     [latitude, longitude] = place.loc.split(',');
     
     LATITUDE_CURRENT_CITY = +latitude;
@@ -280,75 +280,39 @@ function showCurrentWeatherDescribe(weatherData) {
 }
 
 function translateValueWindDirectionDegToCard(deg, LANG) {
-  if (LANG === 'ru') {
       if (deg > 11.25 && deg <= 33.75) {
-        return 'ССВ';
+        return  windDirection[LANG]['NNE'];
       } else if (deg > 33.75 && deg <= 56.25) {
-        return 'СВ';
+        return  windDirection[LANG]['NE'];
       } else if (deg > 56.25 && deg <= 78.75) {
-        return 'ВСВ';
+        return  windDirection[LANG]['ENE'];
       } else if (deg > 78.75 && deg <= 101.25) {
-        return 'В';
+        return  windDirection[LANG]['E'];
       } else if (deg > 101.25 && deg <= 123.75) {
-        return 'ВЮВ';
+        return  windDirection[LANG]['ESE'];
       } else if (deg > 123.75 && deg <= 146.25) {
-        return 'ЮВ';
+        return  windDirection[LANG]['SE'];
       } else if (deg > 146.25 && deg <= 168.75) {
-        return 'ЮЮВ';
+        return  windDirection[LANG]['SSE'];
       } else if (deg > 168.75 && deg <= 191.25) {
-        return 'Ю';
+        return  windDirection[LANG]['S'];
       } else if (deg > 191.25 && deg <= 213.75) {
-        return 'ЮЮЗ';
+        return  windDirection[LANG]['SSW'];
       } else if (deg > 213.75 && deg <= 236.25) {
-        return 'ЮЗ';
+        return  windDirection[LANG]['SW'];
       } else if (deg > 236.25 && deg <= 258.75) {
-        return 'ЗЮЗ';
+        return  windDirection[LANG]['WSW'];
       } else if (deg > 258.75 && deg <= 281.25) {
-        return 'З';
+        return  windDirection[LANG]['W'];
       } else if (deg > 281.25 && deg <= 303.75) {
-        return 'ЗСЗ';
+        return  windDirection[LANG]['WNW'];
       } else if (deg > 303.75 && deg <= 326.25) {
-        return 'СЗ';
+        return  windDirection[LANG]['NW'];
       } else if (deg > 326.25 && deg <= 348.75) {
-        return 'ССЗ';
+        return  windDirection[LANG]['NNW'];
       } else {
-        return 'С';
+        return  windDirection[LANG]['N'];
       }
-  } else {
-      if (deg > 11.25 && deg <= 33.75) {
-        return 'NNE';
-      } else if (deg > 33.75 && deg <= 56.25) {
-        return 'NE';
-      } else if (deg > 56.25 && deg <= 78.75) {
-        return 'ENE';
-      } else if (deg > 78.75 && deg <= 101.25) {
-        return 'E';
-      } else if (deg > 101.25 && deg <= 123.75) {
-        return 'ESE';
-      } else if (deg > 123.75 && deg <= 146.25) {
-        return 'SE';
-      } else if (deg > 146.25 && deg <= 168.75) {
-        return 'SSE';
-      } else if (deg > 168.75 && deg <= 191.25) {
-        return 'S';
-      } else if (deg > 191.25 && deg <= 213.75) {
-        return 'SSW';
-      } else if (deg > 213.75 && deg <= 236.25) {
-        return 'SW';
-      } else if (deg > 236.25 && deg <= 258.75) {
-        return 'WSW';
-      } else if (deg > 258.75 && deg <= 281.25) {
-        return 'W';
-      } else if (deg > 281.25 && deg <= 303.75) {
-        return 'WNW';
-      } else if (deg > 303.75 && deg <= 326.25) {
-        return 'NW';
-      } else if (deg > 326.25 && deg <= 348.75) {
-        return 'NNW';
-      } else {
-        return 'N';
-      }
-  }
 }
 
 function setThreeNextDays(LANG, weatherData) {
